dcfa6d978f525cf1a4351cd06c4eff643be44e52
WooLimKim <imac05@iMac05ui-iMac.local>
2016-11-28 21:03:22 +0900

    sadari app

---
+++ sadariApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  sadariApp
+//
+//  Created by iMac05 on 2016. 11. 11..
+//  Copyright © 2016년 iMac05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
